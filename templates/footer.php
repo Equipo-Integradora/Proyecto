36370018ee511aa478c9b7e1b58a9e53c1dff801
Zanobax <139566870+Zanobax@users.noreply.github.com>
2023-07-14 11:47:36 -0500

    Update database.php
    poniendole footer y header al home

---
+++ templates/footer.php
@@ -0,0 +1,64 @@
+
+      <!-- FOOTER -->
+      <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-3 mx-auto">
+                    <div class="single-box">
+                        <h2>Acceso rápido</h2>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Agendar cita</a></li>
+                        <li><a href="#">Productos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-3 mx-auto">
+                    <div class="single-box">
+                        <h2>Extra links</h2>
+                    <ul>
+                        <li><a href="#">Mi cuenta</a></li>
+                        <li><a href="#">Mis ordenes</a></li>
+                        <li><a href="#">Mis citas</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-3 mx-auto">
+                    <div class="single-box">
+                        <h2>Información de contacto</h2>
+                        <p>8713719607</p>
+                        <p>SweetBeauty@gmail.com </a>
+                        <p>Torreón Coahuila México - 27000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="credit m-5 text-center">Creado por <span>Charly, José, Erick, Lizeth, Fernando</span> | all rigth reserve.</div>
+    </footer>
+      <!-- FIN DE FOOTER -->
+
+      <!-- SCRIPTS -->
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+</script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/home2.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sweet Beauty</title>
+</head>
+<body>
+    <?php
+    session_start();
+    ?>
+    <!-- INICIO DE LA BARRA DE NAVEGACION -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+        <!-- LOGO -->
+        <!-- DURAAAANNNNNNNNNNNNNn -->
+          <a class="navbar-brand logo fs-2" href="#"><span>Sweet </span>Beauty</a>
+        <!-- Toggle btn-->
+          <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <!-- Barra lateral -->
+          <div class="offcanvas offcanvas-start border-bottom" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <!-- Barra lateral header -->
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title logo" id="offcanvasNavbarLabel"><span>Sweet </span>Beauty</h5>
+              <button type="button" class="btn-close shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <!-- Barra lateral body -->
+            <div class="offcanvas-body d-flex flex-column flex-lg-row">
+              <ul class="navbar-nav justify-content-center flex-grow-0 pe-3 align-items-center fa-bars">
+                <li class="nav-item mx-2">
+                  <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#">Agendar cita</a>
+                  </li>
+                  <li class="nav-item mx-2">
+                    <a class="nav-link" href="../scripts/verproductos.php">Productos</a>
+                  </li>
+              </ul>
+              <!-- BARRA DE BUSQUEDA -->
+              <div class="group d-flex justify-content-center align-items-center mx-auto">
+                <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+                <form action="../scripts/buscar_producto.php" method="post">
+                  <input placeholder="Buscar..." type="search" class="input" name="buscar">
+                </form>
+                
+              </div>
+              <!-- BOTONES DE CARRITO E INICIO DE SESION/REGISTRO -->
+              <div class="d-flex justify-content-center align-items-center gap-lg-1 icons">
+                <a href="#" class="bi bi-bag-heart-fill icono1"><a>
+                    <div class="dropdown">
+                        <a class="bi bi-person-fill icono2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-lg-end">
+                          <?php
+                             if(!isset($_SESSION["usuario"]))
+                             {
+                          ?>
+                          <li><a class="dropdown-item fs-6" href="../views/login.php">Iniciar sesion</a></li>
+                          <li><a class="dropdown-item fs-6 " href="../views/registrarse.php">Registrarse</a></li>
+                          <?php
+                            }
+                          ?>
+                          <li><?php
+                                 if(isset($_SESSION["usuario"]))
+                                 {
+                                     echo "<a class='dropdown-item fs-6' href='#'>".$_SESSION["usuario"]."</a>";
+                                 }
+                              ?>
+                        </li>
+                        <li>
+                            <?php
+                                if(isset($_SESSION["usuario"]))
+                                {
+                                    echo "<a class='dropdown-item fs-6 ' href='../scripts/cerrar_sesion.php'>[Cerrar Sesión]</a>";
+                                }
+                              ?>
+                      </li>
+                        </ul>
+                      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- FIN DE LA BARRA DE NAVEGACION -->
