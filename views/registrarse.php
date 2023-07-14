b95d113cf4258c4bae89410d0f0c1c5587050cdf
CharlyMx32 <102246854+CharlyMx32@users.noreply.github.com>
2023-07-14 13:36:56 -0600

    duran puto

--- views/registrarse.php
+++ views/registrarse.php
@@ -11,65 +11,69 @@
 </head>
 <body>
 <div class="wrapper">
-        <div class="container main">
-            <div class="row">
-                <div class="col-md-6 image">
-                    <div class="text">
-                        <p class="logo fs-1 m-auto"><span>Sweet </span>Beauty</p>
-                        <p>Maquillaje y peinados</p>
-                    </div>
-                </div>
-                <div class="col-md-6 right">
-        <form action="../scripts/ingresar_usuario.php" method="post">
-        <header style="text-align: center;font-size: 25px;padding:35px">Registrarse</header>
-            <div class="content">
-                <div class="input-box">
-                    <label for="nombre">Nombre completo</label>
-                    <input type="text" class="input" name="nombre" required autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="correo">Correo</label>
-                    <input type="email" class="input" name="correo" required autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="telefono">Telefono</label>
-                    <input type="tel" class="input" name="telefono" required autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="pass">Contraseña</label>
-                    <input type="password" class="input" name="pass" required autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="passconf">Confirmar contraseña</label>
-                    <input type="password" class="input" name="passconf" required autocomplete="off">
-                </div>
-                <div>
-                <span class="genero-titulo">Genero</span>
-                    <div class="genero-categoria">
-                        <input type="radio" name="genero" value="1">
-                        <label for="genero">Masculino</label><br>
-                        <input type="radio" name="genero" value="2">
-                        <label for="genero">Femenino</label>
-                    </div>
-                </div>
-                <div class="input-box">
-                    <input type="date" name="fecha">
-                    <label for="fecha">Fecha de nacimiento</label>
-                </div>
-                <div class="alert">
-                    <p>Ya tienes cuenta? <a href="../views/login.php">Inicia sesion aqui</a></p>
-                </div>
-                <div class="button-container">
-                    <button type="submit">Registrarse</button>
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 image">
+                <div class="text">
+                    <p class="logo fs-1 m-auto"><span>Sweet </span>Beauty</p>
+                    <p>Maquillaje y peinados</p>
                 </div>
             </div>
-        </form>
-        </div>
+            <div class="col-md-6 right">
+                <form action="../scripts/ingresar_usuario.php" method="post">
+                    <header style="text-align: center;font-size: 25px;padding:35px">Registrarse</header>
+                    <div class="content">
+                        <div class="form-group">
+                            <label for="nombre">Nombre completo</label>
+                            <input type="text" class="form-control" name="nombre" required autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="correo">Correo</label>
+                            <input type="email" class="form-control" name="correo" required autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="telefono">Telefono</label>
+                            <input type="tel" class="form-control" name="telefono" required autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="pass">Contraseña</label>
+                            <input type="password" class="form-control" name="pass" required autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="passconf">Confirmar contraseña</label>
+                            <input type="password" class="form-control" name="passconf" required autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <span class="genero-titulo">Género</span>
+                            <div class="genero-categoria">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="genero" value="1">
+                                    <label class="form-check-label" for="genero">Masculino</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="genero" value="2">
+                                    <label class="form-check-label" for="genero">Femenino</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="date" class="form-control" name="fecha">
+                            <label for="fecha">Fecha de nacimiento</label>
+                        </div>
+                        <div class="alert">
+                            <p>¿Ya tienes una cuenta? <a href="../views/login.php">Inicia sesión aquí</a></p>
+                        </div>
+                        <div class="button-container">
+                            <button type="submit" class="btn btn-primary">Registrarse</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    
-      <!-- SCRIPTS -->
-      <script src="../js/bootstrap.bundle.min.js"></script>
+</div>
+
+<!-- SCRIPTS -->
+<script src="../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
