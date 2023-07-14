f3bfc19149a9d16e6ca3c27441792592e900df0f
CharlyMx32 <102246854+CharlyMx32@users.noreply.github.com>
2023-07-14 13:29:46 -0600

    oli
    .l.
    
    Co-Authored-By: Durann23 <139575570+Durann23@users.noreply.github.com>

--- views/registrarse.php
+++ views/registrarse.php
@@ -10,38 +10,48 @@
     <title>Registrarse</title>
 </head>
 <body>
-    <div class="containter">
+<div class="wrapper">
+        <div class="container main">
+            <div class="row">
+                <div class="col-md-6 image">
+                    <div class="text">
+                        <p class="logo fs-1 m-auto"><span>Sweet </span>Beauty</p>
+                        <p>Maquillaje y peinados</p>
+                    </div>
+                </div>
+                <div class="col-md-6 right">
         <form action="../scripts/ingresar_usuario.php" method="post">
-            <h2 class="fs-1">Sweet Beauty</h2>
-            <h4 class="fs-5">Registrarse</h4>
+        <header style="text-align: center;font-size: 25px;padding:35px">Registrarse</header>
             <div class="content">
                 <div class="input-box">
-                    <input type="text" class="input" name="nombre" required autocomplete="off">
                     <label for="nombre">Nombre completo</label>
+                    <input type="text" class="input" name="nombre" required autocomplete="off">
                 </div>
                 <div class="input-box">
-                    <input type="email" class="input" name="correo" required autocomplete="off">
                     <label for="correo">Correo</label>
+                    <input type="email" class="input" name="correo" required autocomplete="off">
                 </div>
                 <div class="input-box">
-                    <input type="tel" class="input" name="telefono" required autocomplete="off">
                     <label for="telefono">Telefono</label>
+                    <input type="tel" class="input" name="telefono" required autocomplete="off">
                 </div>
                 <div class="input-box">
-                    <input type="password" class="input" name="pass" required autocomplete="off">
                     <label for="pass">Contraseña</label>
+                    <input type="password" class="input" name="pass" required autocomplete="off">
                 </div>
                 <div class="input-box">
-                    <input type="password" class="input" name="passconf" required autocomplete="off">
                     <label for="passconf">Confirmar contraseña</label>
+                    <input type="password" class="input" name="passconf" required autocomplete="off">
                 </div>
+                <div>
                 <span class="genero-titulo">Genero</span>
                     <div class="genero-categoria">
                         <input type="radio" name="genero" value="1">
-                        <label for="genero">Masculino</label>
+                        <label for="genero">Masculino</label><br>
                         <input type="radio" name="genero" value="2">
                         <label for="genero">Femenino</label>
                     </div>
+                </div>
                 <div class="input-box">
                     <input type="date" name="fecha">
                     <label for="fecha">Fecha de nacimiento</label>
@@ -54,6 +64,9 @@
                 </div>
             </div>
         </form>
+        </div>
+            </div>
+        </div>
     </div>
     
       <!-- SCRIPTS -->
