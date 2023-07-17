9bb819b8791520d75c7f275d9a54c5cfda39f756
fernandom11399 <jafetavalos49@gmail.com>
2023-07-16 22:49:22 -0600

    Update buscar_producto.php

--- scripts/buscar_producto.php
+++ scripts/buscar_producto.php
@@ -3,62 +3,99 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Productos Generales</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/productos_gene.css">
 </head>
 <body>
-    <div class="container">
-        <h1 align="center">Coincidencias</h1>
-        <form action="../scripts/verfiltradocliente.php" method='post' style="width:auto margin=auto" >
+    <?php
+    include "../templates/header.php";
+    include "../class/database.php";
+    extract($_POST);
+
+    $conexion = new database();
+    $conexion->conectarDB();
+
+        $consulta = "
+        SELECT productos.nombre_producto, productos.precio_producto, detalle_productos.existencias_detalle_producto, detalle_productos.imagen_detalle_producto
+    FROM detalle_productos INNER JOIN productos ON productos.id_producto = detalle_productos.detalle_producto_detalle_producto_FK
+    INNER JOIN tipo_categorias ON tipo_categorias.id_tipo_categoria = productos.categoria_producto_FK
+    Where productos.nombre_producto LIKE '%$buscar%';";
+
+    $t = $conexion->busca($consulta);
+
     
-        <?php
-            include "../class/database.php";
-            $conexion = new Database();
-            $conexion->conectarDB();
+    $consulta1 = "SELECT *
+    FROM categorias;";
+    $cat = $conexion->cate($consulta1);
 
-            extract($_POST);
-            $consultapro ="SELECT productos.nombre_producto, productos.precio_producto, tipo_categorias.nombre_tipo_categoria, detalle_productos.existencias_detalle_producto, detalle_productos.imagen_detalle_producto
-            FROM detalle_productos INNER JOIN productos ON productos.id_producto = detalle_productos.detalle_producto_detalle_producto_FK
-            INNER JOIN tipo_categorias ON tipo_categorias.id_tipo_categoria = productos.categoria_producto_FK
-            Where productos.nombre_producto LIKE '%$buscar%';";
+    
 
-            $tabla = $conexion->seleccionar($consultapro);
 
-echo "<table class='table table-hover'>
-<thead class='table-dark'>
-    <tr>
-        <th>Producto</th>
-        <th>Precio</th>
-        <th>Tipo De Categoria</th>
-        <th>Existencias</th>
-        <th>Imagen</th>
-    </tr>
-</thead>
-<tbody>";
+    ?>
 
-foreach($tabla as $reg)
-{
-    echo "<tr>";
-    echo "<td> $reg->nombre_producto</td>";
-    echo "<td> $reg->precio_producto</td>";
-    echo "<td> $reg->nombre_tipo_categoria</td>";
-    echo "<td> $reg->existencias_detalle_producto</td>";
-    echo "<td> <img style='height: auto; width: 5rem;' src='/INTEGRADORA/img/productos/".$reg->imagen_detalle_producto."' alt=".$reg->imagen_detalle_producto."></td>";
-    echo "</tr>";
+<div class="container">
+    <h1 class="titu">Coincidencias</h1>
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="container">
+                <h2>Categorías</h2>
+                <ul class='list-group'>
+                <?php
+                foreach ($cat as $index => $ca) {
+                    echo "<li class='list-group-item' data-toggle='collapse' data-target='#subtemas$index'>";
+                    echo "$ca->nombre_categoria";
+                    echo "</li>";  
+                    $consulta2 = "SELECT tipo_categorias.nombre_tipo_categoria
+                        FROM tipo_categorias INNER JOIN categorias ON categorias.id_categoria = tipo_categorias.categoria_tipo_catergoria_FK
+                        WHERE categorias.id_categoria = '$ca->id_categoria';";
+                    $cat_ti = $conexion->cat_tip($consulta2);
+                    echo "<div id='subtemas$index' class='collapse'>";
+                    echo "    <ul class='list-group'>";
+                    foreach ($cat_ti as $c) {
+                        echo "        <li class='list-group-item'>".$c->nombre_tipo_categoria."</li>";
+                    }                    
+                    echo "    </ul>";
+                    echo "</div>";
+                }
+                ?>
+                </ul>
+            </div>
+        </div>
+
+            <div class="col-lg-9">
+                <div class="row">
+                    
+                    <?php
+foreach ($t as $r) {
+    echo "<div class='col-lg-4 '>";
+    echo "<div class='card'>";
+    echo "<img class='card-img-top pro' src='../img/productos/".$r->imagen_detalle_producto."' alt='...'>";
+    echo "<div class='card-body text-center'>";
+    echo "<div class='icons card-title'>"; 
+    echo "</div>";
+    echo "<div class='card-text'>";
+    echo "<h3 class='product-title'>".$r->nombre_producto."</h3>";
+    echo "<div class='price'>";
+    echo "$".$r->precio_producto;
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
 }
-echo "</tbody>";
-echo "</table>";
 $conexion->desconectarDB();
+?>
 
 
-        ?>
-        </form>
-        
-
-
+                </div>
+                
+            </div>
+        </div>
     </div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../js/listas.js"></script>
 </body>
 </html>
-
-
