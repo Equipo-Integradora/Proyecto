132ce44d565815372b5b282c38ec7340e99990cf
fernandom11399 <jafetavalos49@gmail.com>
2023-07-17 21:32:32 -0600

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -28,7 +28,7 @@
                     <div class="single-box">
                         <h2>Información de contacto</h2>
                         <ul>
-                        <li><a href="https://api.whatsapp.com/send?phone=8713719607">8713719607</a></li>
+                        <li><a href="https://api.whatsapp.com/send?phone=8713719607">WhatsApp</a></li>
                         <li><a href="mailto:sweetbeautyutt@gmail.com">SweetBeautyUtt@gmail.com</a></li>
                         <li><a href="https://www.google.com/maps?q=25.602931834354884,-103.37203726085112">Torreón Coahuila México - 27000</a></li>
                     </ul>
