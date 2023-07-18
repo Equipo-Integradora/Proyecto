785d920e96078d0ee3f4e77972c021733fb27777
fernandom11399 <jafetavalos49@gmail.com>
2023-07-18 12:46:16 -0600

    Update registrarse.php

--- views/registrarse.php
+++ views/registrarse.php
@@ -78,6 +78,10 @@
                                     <input class="form-check-input" type="radio" name="genero" value="2">
                                     <label class="form-check-label" for="genero">Femenino</label>
                                 </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="genero" value="3">
+                                    <label class="form-check-label" for="genero">Otro</label>
+                                </div>
                             </div>
                         </div>
                         <div class="form-group" id="fecha-group">
