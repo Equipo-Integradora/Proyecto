c2238776a7f3da238024200afcc4323ecfc051d0
Jess JH <lizethjh17@gmail.com>
2023-07-18 19:44:47 -0600

    INTERFAZ DEL ADMIN :cake:
    Ahí me dicen que tal, si le cambio algo o no sé, ustedes díganme.
    
    LOS TKM :blue_heart:

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const monthNames = ["Enero", "Febreri", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Deciembre"
+];
+
+const interval = setInterval(() => {
+
+    const local = new Date();
+    
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthNames[month]} ${year}`;
+
+}, 1000);
