417dcdd4c2d2ca4fa6c87b334b8da7180d31302c
Jess JH <lizethjh17@gmail.com>
2023-07-18 19:48:39 -0600

    Update admin.php
    Ya es el ultimo, es que trabaje todo aparte

--- views/admin.php
+++ views/admin.php
@@ -101,7 +101,7 @@
     </div>
 
     <!-- SCRIPTS -->
-    <script src="../prueba/js/clock.js"></script>
+    <script src="../js/clock.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         var el = document.getElementById("wrapper");
