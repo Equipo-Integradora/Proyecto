fcfae5acdd93e1343ef9f3e00599a369bb576b92
fernandom11399 <jafetavalos49@gmail.com>
2023-07-30 19:38:06 -0600

    Update ingresar_cita.php

--- scripts/ingresar_cita.php
+++ scripts/ingresar_cita.php
@@ -8,17 +8,17 @@ $db->conectarDB();
 
 extract($_POST);
 
-$registro = "INSERT INTO registros_cita(cliente_registro_cita_FK, fecha_cita_registro_cita, hora_registro_cita, descripcion_registro_cita)
-VALUES ('{$_SESSION["id"]}', 'selectedDate', 'selectedTime', 'descripcion_registro_cita');";
-$db->ejecuta($registro);
+    $registro = "INSERT INTO registros_cita(cliente_registro_cita_FK, fecha_cita_registro_cita, hora_registro_cita, descripcion_registro_cita)
+    VALUES ('{$_SESSION["id"]}', 'selectedDate', 'selectedTime', 'descripcion_registro_cita');";
+    $db->ejecuta($registro);
 
-$registro = $db->ultimaid();
+    $registro = $db->ultimaid();
 
-foreach($servicio as $ser)
-{
- $detalle = "INSERT INTO detalles_registros_cita(registro_cita_detalle_registro_FK, tipo_servicio_registro_cita_FK, precio_registro_cita)
- VALUES('$registro', '$ser', '$precio');";
-}
+    foreach($servicio as $ser)
+    {
+    $detalle = "INSERT INTO detalles_registros_cita(registro_cita_detalle_registro_FK, tipo_servicio_registro_cita_FK, precio_registro_cita)
+    VALUES('$registro', '$ser', '$precio');";
+    }
 
 $db->ejecuta($detalle);
 $db->desconectarDB();
