293b635b4c32b1f3ba0659c61aba70bbd400ada7
fernandom11399 <jafetavalos49@gmail.com>
2023-08-12 09:20:45 -0600

    Lo cambia todo :anguished:

--- views/registrarse.php
+++ views/registrarse.php
@@ -9,6 +9,7 @@ if(!isset($_SESSION["usuario"]))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/home/logo.png" type="logo1/png">
     <link rel="stylesheet" href="../css/registrarse.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
