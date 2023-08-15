541daa2bfc2adc29a1be629a0adf91f47ff827e3
Durann23 <jd43573@gmail.com>
2023-08-15 11:29:13 -0600

    cancelar citas

--- scripts/admincita.php
+++ scripts/admincita.php
@@ -7,6 +7,26 @@ if(isset($_SESSION["admin"]))
 include "../templates/sidebar.php";
 $conexion = new database();
 $conexion->conectarDB();
+
+
+$fechita=date("y-m-d");
+    $consulta = "SELECT * 
+    FROM registros_cita
+    where estado_registro_cita='Aceptada'";
+    $no=$conexion->seleccionar($consulta);
+    foreach($no as $sis){
+        $orden=$sis->id_registro_cita;
+        $fechabd=$sis->fecha_creacion_registro_cita;
+        $diferenciaDias = floor((strtotime($fechita) - strtotime($fechabd)) / (60 * 60 * 24));
+    if($diferenciaDias>=1){
+        $caducar="update registros_cita
+        set
+        estado_registro_cita='Cancelada'
+        where id_registro_cita='$orden'";
+        $resultado=$conexion->ejecuta($caducar);
+    }
+    }
+
 ?>
 
     <div class="text-center">
